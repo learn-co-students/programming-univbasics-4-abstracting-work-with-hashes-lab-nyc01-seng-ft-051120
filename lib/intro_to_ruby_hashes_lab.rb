9aea69d369e1ba2c952e49764b5642d798a52f43
Christa Gammage <crgammage@gmail.com>
2020-04-22 22:06:19 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  new_hash = Hash.new
+  # new_hash = Hash.new
   new_hash = {key => value}
   new_hash
 end
